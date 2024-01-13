4e9bf33e25b395a5b00f02684f39e4cb3a6758c8
murilo-bujule <114461335+murilo-bujule@users.noreply.github.com>
2024-01-13 14:35:38 +0000

    Usando Repl.it + jogo da adivinhacao completo + inicio jogo da forca
    - Passei a usar o Repl.it
    - Finalizei o jogo da adivinhação da Alura com modificações (ainda mais modificações serão bem-vindas)
    - Iniciei o jogo da forca.

---
+++ Curso_01/Jogo_forca/forca.cpp
@@ -0,0 +1,14 @@
+/*************************************************
+ALGORITMO: JOGO DA FORCA
+ENTRADA: Palavra a ser adivinhada
+SAÍDA: Palavra a ser adivinhada com as letras descobertas
+**************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string palavra_secreta = "MELANCIA";
+  cout << palavra_secreta << endl;
+}
