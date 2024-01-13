ccd297572a03821701e8cbb504e1cb5c04493dc0
murilo-bujule <114461335+murilo-bujule@users.noreply.github.com>
2024-01-13 14:32:31 +0000

    Finalizado o jogo da adivinhacao (0 refatoracoes)
    Jogo da adivinhação finalizado de acordo com o curso da Alura.
    Ter atenção na mudança onde meu código não aceita entrada inválida para escolha do nível de dificuldade do jogo.
    Planejo fazer alterações futuras para colocar tudo em funções próprias, retirando quase tudo da função main.

--- Curso_01/Jogo_adivinhacao/jogoadivinhacao.cpp
+++ Curso_01/Jogo_adivinhacao/jogoadivinhacao.cpp
@@ -1,45 +1,78 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
 
-    cout << "**************************************" << endl;
-    cout << "* Bem-vindos ao jogo da adivinhação! *" << endl;
-    cout << "**************************************" << endl;
+  cout << "**************************************" << endl;
+  cout << "* Bem-vindos ao jogo da adivinhação! *" << endl;
+  cout << "**************************************" << endl;
+  cout << "Patorico" << endl;
 
-    const int NUMERO_SECRETO = 42;
+  cout << "Escolha o seu nível de dificuldade:" << endl;
+  cout << "Fácil (F), Médio (M) ou Difícil (D)" << endl;
 
-    bool nao_acertou = true;
-    int tentativas = 0;
+  char dificuldade;
+  cin >> dificuldade;
 
-    while (nao_acertou)
-    {   
-        tentativas++;
-        int chute;
-        cout << "Tentativa: " << tentativas << endl;
-        cout << "Qual seu chute? ";
-        cin >> chute;
+  int numero_de_tentativas;
 
-        cout << "O valor do seu chute é: " << chute << endl;
-
-        bool acertou = (chute == NUMERO_SECRETO);
-        bool maior = (chute > NUMERO_SECRETO);
-
-        if (acertou)
-        {
-            cout << "Parabéns! Você acertou o número secreto!" << endl;
-            nao_acertou = false;
-        }
-        else if (maior)
-        {
-            cout << "Seu chute foi maior que o número secreto!" << endl;
-        }
-        else
-        {
-            cout << "Seu chute foi menor que o número secreto!" << endl;
-        }
+  while (true) {
+    if (dificuldade == 'F' || dificuldade == 'f') {
+      numero_de_tentativas = 15;
+      break;
+    } else if (dificuldade == 'M' || dificuldade == 'm') {
+      numero_de_tentativas = 10;
+      break;
+    } else if (dificuldade == 'D' || dificuldade == 'd') {
+      numero_de_tentativas = 5;
+      break;
+    } else {
+      cout << "Dificuldade inválida!" << endl;
     }
-    cout << "Fim de jogo!" << endl;
-    cout << "Você acertou o número secreto em " << tentativas << " tentativas!" << endl;
+  }
+
+  srand(time(NULL));
+  const int NUMERO_SECRETO = rand() % 100;
+
+  bool nao_acertou = true;
+  int tentativas = 0;
+  double pontos = 1000.0;
+
+  for (tentativas = 1; tentativas <= numero_de_tentativas; tentativas++) {
+    int chute;
+    cout << "Tentativa: " << tentativas << endl;
+    cout << "Qual seu chute? ";
+    cin >> chute;
+
+    double pontos_perdidos = abs(chute - NUMERO_SECRETO) / 2.0;
+    pontos = pontos - pontos_perdidos;
+
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    bool acertou = (chute == NUMERO_SECRETO);
+    bool maior = (chute > NUMERO_SECRETO);
+
+    if (acertou) {
+      cout << "Parabéns! Você acertou o número secreto!" << endl;
+      nao_acertou = false;
+      break;
+    } else if (maior) {
+      cout << "Seu chute foi maior que o número secreto!" << endl;
+    } else {
+      cout << "Seu chute foi menor que o número secreto!" << endl;
+    }
+  }
+  cout << "Fim de jogo!" << endl;
+  if (nao_acertou) {
+    cout << "Você perdeu! Tente novamente!" << endl;
+  } else {
+    cout << "Você acertou o número secreto em " << tentativas << " tentativas!"
+         << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
+
+  }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Alura_cursos
 Cursos feitos na plataforma Alura
+
+## Curso 01: C++: Conhecendo a linguagem e a STL
+
+<h3>Primeiro curso da formação C++:</h3>
+<ul>
+  <li>Criar um jogo da adivinhação</li>
+  <li>Criar um jogo da forca</li>
+</ul>
